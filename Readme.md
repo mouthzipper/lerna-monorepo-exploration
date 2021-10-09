3252e42514a68b040b4af57450a8ad665fb08e74
Jerome Ramos <ramos.jeromeh@gmail.com>
2021-10-09 12:23:04 +0800

    configure lerna with 2 packages with correct script

---
+++ Readme.md
@@ -0,0 +1,2 @@
+`npx lerna clean -y` - clean up all packages node_modules
+`npx lerna boostrap --hoist` - install all dependencies that are required for all the packages
---
+++ packages/server-api/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hey, server here!");
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`Error: ${err.message}`);
+  } else {
+    console.log(`Listening on port ${port}`);
+  }
+});
